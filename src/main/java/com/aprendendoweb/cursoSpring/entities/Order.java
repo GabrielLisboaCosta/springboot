bfdf78c167bd4aa8ee1a12db628016aa46dc1775
Gabriel Lisboa <gabrielcosta1055@hotmail.com>
2024-03-27 14:25:11 -0300

    Subtotal & Total methods

--- src/main/java/com/aprendendoweb/cursoSpring/entities/Order.java
+++ src/main/java/com/aprendendoweb/cursoSpring/entities/Order.java
@@ -31,7 +31,7 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-
+ 
 	private Integer orderStatus;
 
 	@ManyToOne
@@ -100,6 +100,14 @@ public class Order implements Serializable {
 
 	public void setPayment(Payment payment) {
 		this.payment = payment;
+	}
+	
+	public Double getTotal() {
+		double sum = 0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal(); 
+		}
+		return sum;
 	}
 
 	@Override
