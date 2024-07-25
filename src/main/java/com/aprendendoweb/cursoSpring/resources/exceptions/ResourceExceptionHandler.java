c00d2f0ea42ac6d33b26c0326c43817d01458076
Gabriel Lisboa <gabrielcosta1055@hotmail.com>
2024-07-25 10:26:21 -0300

    exceções do delete n estão funcionando

--- src/main/java/com/aprendendoweb/cursoSpring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/aprendendoweb/cursoSpring/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.aprendendoweb.cursoSpring.resources.exceptions;
 
 import java.time.Instant;
 
+import com.aprendendoweb.cursoSpring.services.exceptions.DatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource nor found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
